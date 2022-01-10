a2afe1a2108f756457d8b79f3fc6378a5ea45854
sunee <1250432262@qq.com>
2022-01-10 19:22:18 +0800

    PC+移动

--- main.js
+++ main.js
@@ -1,27 +1,22 @@
 $(function () {
-  let tag = 0;
+  let tag = 1;
   const $lastLi = $(".addTag");
-  $(".mainPart").on("click", function () {
+  $("body").click(function () {
     $(".close").css("display", "none");
     tag = 0;
   });
-  /*
-   *打开页面读取localStorage里面的数据，并利用JSON转换为JS对象数据
-   */
   const x = localStorage.getItem("x");
   const newString = JSON.parse(x);
 
   const hashMap = newString || [
     { logo: "A", url: "https://www.acfun.cn" },
     { logo: "B", url: "https://www.bilibili.com" },
+    { logo: "C", url: "https://www.acfun.cn" },
+    { logo: "D", url: "https://www.bilibili.com" },
   ];
 
   const render = function () {
-    console.log("3:");
     $("li:not(.addTag)").remove(), //移除add前的所有标签
-      /*
-       *给每个hashMap的item生成一个li标签，并按顺序插在add标签前
-       */
       hashMap.forEach(function (item, index) {
         let $li = $(`<li class="addChidren">
         <div class="close">
@@ -35,45 +30,92 @@ $(function () {
         </li>`);
 
         $li.insertBefore($lastLi);
+        if (tag === 0) {
+          $(".tagList").find(".close").css("display", "none");
+        } else {
+          $(".tagList").find(".close").css("display", "block");
+        }
+        // PC端鼠标相关事件
+        $li.mouseover(() => {
+          $(".tagList").find(".close")[index].style.display = "block";
+        });
+        $li.mouseout(() => {
+          $(".tagList").find(".close")[index].style.display = "none";
+        });
+        $li.on("click", function () {
+          window.open(item.url);
+        });
+        $li.on("click", ".close", (e) => {
+          e.stopPropagation();
+          hashMap.splice(index, 1);
+          tag = 1;
+          render();
+        });
 
-        $li.on("touchstart", function () {
-          // 长按事件触发
-          timeOutEvent = setTimeout(function () {
+        // 手机touch相关事件
+        $li.on({
+          touchstart: function () {
+            timeOutEvent = setTimeout(function () {
+              tag = 1; //此处为长按事件-----在此删除按钮
+              render();
+            }, 500);
+          },
+          touchmove: function () {
+            clearTimeout(timeOutEvent);
             timeOutEvent = 0;
-            $(".tagList").find(".close").css("display", "block");
-            tag = 1;
-          }, 400);
+            e.preventDefault();
+          },
+          touchend: function (e) {
+            clearTimeout(timeOutEvent);
+            if (timeOutEvent != 0 && tag === 0) {
+              window.open(item.url); //此处为点击事件----在此处添加跳转链接
+            }
+            return false;
+          },
         });
-        $li.on("touchend", function () {
-          if (tag === 0) {
-            window.open(item.url);
-          }
-        });
-        $(".close").on("touchend", function (e) {
-          if (tag === 1) {
-            e.stopPropagation();
-            hashMap.splice(index, 1);
-            console.log("2:");
-            render();
-            tag = 0;
-          }
+
+        $(".close").on({
+          touchstart: function (e) {},
+          touchmove: function () {
+            clearTimeout(timeOutEvent);
+            e.preventDefault();
+          },
+          touchend: function (e) {
+            clearTimeout(timeOutEvent);
+            if (timeOutEvent != 0) {
+              hashMap.splice(index, 1);
+              tag = 1;
+              render();
+            }
+            return false;
+          },
         });
       });
   };
 
   render(); //刷新页面就执行一次移除旧标签，重新插入的过程
-
-  $lastLi.on("click", function (e) {
-    let url = prompt("请输入你想要的网址");
-    if (url.indexOf("https") !== 0) {
-      if (url.indexOf("www.") !== 0) {
-        url = "https://www." + url;
-      } else {
-        url = "https://" + url;
+  let lastLiEvent = function () {
+    if (tag === 0) {
+      let url = prompt("请输入你想要的网址");
+      if (url.indexOf("https") !== 0) {
+        if (url.indexOf("www.") !== 0) {
+          url = "https://www." + url;
+        } else {
+          url = "https://" + url;
+        }
       }
+      hashMap.push({
+        logo: `${url[12].toLocaleUpperCase()}`,
+        url: `${url}`,
+      }); //每次点击add后，将新数据存入hashMap，生成新的hashMap
+      render();
     }
-    hashMap.push({ logo: `${url[12].toLocaleUpperCase()}`, url: `${url}` }); //每次点击add后，将新数据存入hashMap，生成新的hashMap
-    render();
+  };
+  $lastLi.on("click", () => {
+    lastLiEvent();
+  });
+  $lastLi.on("touchend", () => {
+    lastLiEvent();
   });
 
   /*
